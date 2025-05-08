bfe6074529ab3384214dfc740d986fcad2625bc2
Sain-Biswas <sain.biswas614@gmail.com>
2025-05-08 16:09:39 +0530

    feat(eslint-config): changed eslint into a package import

---
+++ packages/eslint-config/index.ts
@@ -0,0 +1,3 @@
+export { default as apiEslintConfig } from "./eslint.api";
+export { default as reactEslintConfig } from "./eslint.react";
+export { default as baseEslintConfig } from "./eslint.base";
