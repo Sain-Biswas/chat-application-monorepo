6eb5afb069f246244d88433d78754a3336d37ca7
Sain-Biswas <sain.biswas614@gmail.com>
2025-05-11 13:41:46 +0530

    feat(api): add basic openapi doc with swaggerui support

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,44 @@
-import { Hono } from "hono";
+import { swaggerUI } from "@hono/swagger-ui";
+import { createRoute, OpenAPIHono, z } from "@hono/zod-openapi";
 
-const app = new Hono();
+const app = new OpenAPIHono();
 
-app.get("/", (c) => c.text("Hello Hono!"));
+app.openapi(
+  createRoute({
+    method: "get",
+    path: "/api",
+    responses: {
+      200: {
+        content: {
+          "application/json": {
+            schema: z.object({
+              message: z.string(),
+            }),
+          },
+        },
+        description: "Success Response",
+      },
+    },
+    description: "Home Route",
+  }),
+  (c) => {
+    return c.json({ message: "Hello World!" }, 200);
+  }
+);
+
+app.get(
+  "/api/ui",
+  swaggerUI({
+    url: "/api/doc",
+  })
+);
+
+app.doc("/api/doc", {
+  openapi: "3.1.0",
+  info: {
+    version: "0.0.1",
+    title: "Zaptalk",
+  },
+});
 
 export default app;
