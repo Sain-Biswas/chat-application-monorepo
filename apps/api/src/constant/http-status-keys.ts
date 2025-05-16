6222865a037b3be7d9bc2e710edafc6b05db0059
Sain-Biswas <sain.biswas614@gmail.com>
2025-05-17 01:23:48 +0530

    feat(api): add http status codes and phrases

---
+++ apps/api/src/constant/http-status-keys.ts
@@ -0,0 +1,5 @@
+import HTTPStatusCodes from "./http-status-codes";
+
+const HTTPStatusKeys = Object.keys(HTTPStatusCodes);
+
+export default HTTPStatusKeys;
--- apps/api/src/validator/default-error.ts
+++ apps/api/src/validator/default-error.ts
@@ -1,13 +1,16 @@
 import z from "zod";
+import HTTPStatusKeys from "../constant/http-status-keys";
+
+const ErrorNames = ["SERVER_ERROR", "DATABASE_ERROR", "AUTHENTICATION_ERROR"];
 
 const defaultErrorSchema = z.object({
   success: z["boolean"]()["default"](false),
   error: z.object({
     message: z.string(),
     detail: z.string(),
-    name: z["enum"](["SERVER_ERROR", "DATABASE_ERROR", "AUTHENTICATION_ERROR"]),
+    name: z.enum(ErrorNames),
   }),
-  code: z["enum"](["INTERNAL_SERVER_ERROR"]),
+  code: z.enum(HTTPStatusKeys),
   timestamp: z.date()["default"](new Date()),
   path: z.string(),
 });
