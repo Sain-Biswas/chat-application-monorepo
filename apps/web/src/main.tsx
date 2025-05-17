b6db58dd6c517c4db985a417d4882f6bc638e82f
Sain-Biswas <sain.biswas614@gmail.com>
2025-05-17 17:24:28 +0530

    feat(web): setup tanstack router

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,12 +1,28 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "@zaptalk/ui/styles/global.scss";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./app.tsx";
-import "./index.css";
+import ReactDOM from "react-dom/client";
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-createRoot(document.querySelector("#root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  );
+}
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    TanStackRouterVite({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler", { target: "19" }]],
