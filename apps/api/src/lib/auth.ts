0e08612d2a5ebdccc4e159cfdc919b142d1ed3a7
Sain-Biswas <sain.biswas614@gmail.com>
2025-05-18 22:04:24 +0530

    feat(api): add google oauth support

--- apps/api/src/lib/auth.ts
+++ apps/api/src/lib/auth.ts
@@ -2,6 +2,7 @@ import databaseClient from "@/api/database/index.database";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { openAPI } from "better-auth/plugins";
+import env from "../constant/env";
 
 export const auth = betterAuth({
   database: drizzleAdapter(databaseClient, {
@@ -9,6 +10,12 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    },
   },
   plugins: [openAPI()],
 });
