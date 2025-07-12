31920fbcde0abaf937e1bb1680d07ef47eaa59e0
Sain-Biswas <sain.biswas614@gmail.com>
2025-07-12 23:00:53 +0530

    feat: add signup functionality complete

--- apps/api/src/lib/auth.ts
+++ apps/api/src/lib/auth.ts
@@ -3,10 +3,17 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { openAPI } from "better-auth/plugins";
 import env from "../constant/env";
 import databaseClient from "../database/index.database";
+import { accountSchema, sessionSchema, userSchema, verificationSchema } from "../database/schema/index.schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(databaseClient, {
     provider: "pg",
+    schema: {
+      user: userSchema,
+      session: sessionSchema,
+      account: accountSchema,
+      verification: verificationSchema,
+    },
   }),
   emailAndPassword: {
     enabled: true,
